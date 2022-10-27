caabb7af4666bb6f7c4fd7624e2730eab9f9189f
Ravinder yadav <ravinder.y@crowdbotics.com>
2022-10-27 17:22:14 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,45 @@
+const styles = StyleSheet.create({
+  "hr": {
+    "marginTop": 20,
+    "marginBottom": 20,
+    "borderBottomColor": "black",
+    "borderBottomWidth": 1
+  },
+  "container": {
+    "flex": 1,
+    "height": 100,
+    "padding": 13
+  },
+  "text": {
+    "color": "black",
+    "fontSize": 20
+  },
+  "buttonPressed": {
+    "backgroundColor": "aquamarine"
+  },
+  "buttonNotPressed": {
+    "backgroundColor": "blue"
+  },
+  "button": {
+    "borderRadius": 4,
+    "padding": 15,
+    "marginTop": 10
+  },
+  "buttonText": {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 16
+  }
+});
 export const globalOptions = {
-  name: "testing_new_34545_d_41169",
-  url: "https://testing_new_34545_d_41169.botics.co",
-  api: "https://testing_new_34545_d_41169.botics.co/api/v1"
+  "name": "testing_new_34545_d_41169",
+  "url": "https://testing_new_34545_d_41169.botics.co",
+  "api": "https://testing_new_34545_d_41169.botics.co/api/v1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    "title": "App Load",
+    "copy": "Routes codes",
+    "styles": styles
   }
 };
